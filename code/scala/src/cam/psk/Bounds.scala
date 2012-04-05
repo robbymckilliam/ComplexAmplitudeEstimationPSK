9b5d1df413811fc035e0d3fb9c44c84018f6c65a
Robby McKilliam <robby.mckilliam@gmail.com>
2012-04-05 16:02:59 +0930

    Added a file for computed estimator bounds.  Have put Bill's BPSK bound in there.

---
+++ code/scala/src/cam/psk/Bounds.scala
@@ -0,0 +1,28 @@
+/*
+ * Classes for computing estimator bounds for noncoherent PSK carrier phase estimation
+ */
+
+package cam.psk
+
+import numbers.finite.integration.RealIntegral
+
+trait CramerRaoBound {
+  /** Returns a bound on the estimator variance given noise variance v and number of symbols L*/
+  def variance(v : Double, L : Int) : Double
+}
+
+/** 
+ * This is the bound derived by Bill Cowley.
+ */
+class BPSKCRB extends CramerRaoBound {
+  
+  def variance(v : Double, L : Int) : Double = {  
+    return RealIntegral.trapezoidal( n => f(v,n), -100.0, 100.0, 1000000) 
+  }
+  
+  private def f(v : Double, n : Double) : Double = {
+    val tan = scala.math.tanh((1+n)/v)
+    val exp = scala.math.exp(-n*n/2/v)
+    return tan*tan*exp/scala.math.sqrt(2*scala.math.Pi*v)
+  }
+}
