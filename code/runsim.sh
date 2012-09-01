911be57431d6ac11af412f3cb43184b1d7cfef31
Robby McKilliam <robby.mckilliam@gmail.com>
2012-09-01 17:51:39 +0930

    minor edits to java options, should make the simulation go a little faster.

--- code/runsim.sh
+++ code/runsim.sh
@@ -1,4 +1,5 @@
-#export JAVA_OPTS="-Xprof"
+#export JAVA_OPTS="-Xprof -server -Xms1g -Xmx1g"
+export JAVA_OPTS="-d64 -server -Xms1g -Xmx1g"
 scala -nocompdaemon -cp lib/ComplexAmplitudePSK.jar:lib/ScalaNumbers.jar:lib/PubSim.jar:lib/Jama-1.0.2.jar:lib/flanagan.jar:lib/colt.jar:lib/RngPack.jar clt.scala
 scala -nocompdaemon -cp lib/ComplexAmplitudePSK.jar:lib/ScalaNumbers.jar:lib/PubSim.jar:lib/Jama-1.0.2.jar:lib/flanagan.jar:lib/colt.jar:lib/RngPack.jar montecarlo.scala
 scala -nocompdaemon -cp lib/ComplexAmplitudePSK.jar:lib/ScalaNumbers.jar:lib/PubSim.jar:lib/Jama-1.0.2.jar:lib/flanagan.jar:lib/colt.jar:lib/RngPack.jar montecarlonc.scala
