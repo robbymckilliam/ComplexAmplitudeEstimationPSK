d540539351bec3fef7290df26a14c417c2c3fa71
robbymckilliam <robby.mckilliam@gmail.com>
2014-02-05 14:18:51 +1030

    added QPSK crb

--- code/crb.scala
+++ code/crb.scala
@@ -4,6 +4,8 @@
 import cam.psk.MackenthunCoherent
 import cam.psk.ComplexAmplitudeEstimator
 import cam.psk.BPSKCRB
+import cam.psk.QPSKCRB
+import cam.psk.CramerRaoBound
 import cam.noise.ComplexGaussian
 import numbers.finite.RectComplex
 import numbers.finite.Complex
@@ -11,39 +13,42 @@ import numbers.finite.PolarComplex
 import pubsim.distributions.complex.ComplexRandomVariable
 import pubsim.Util
 
-val Ls = List(32,256,2048,4096)
+/** Compute given CRB for given L and snrs */
+def computecrb( crbf : ()=>CramerRaoBound, L : Int, snrdbs : Seq[Double], name : String) { 
+  
+  val SNRs = snrdbs.map(db => scala.math.pow(10.0, db/10.0))
+  val vars = SNRs.map( snr => 1.0/snr/2 ) //variance for real and imaginay parts (divide by 2)
 
-//construct an array of noise distributions with a logarithmic scale
-val SNRdBs = -20.0 to 20.0 by 0.5
-val SNRs = SNRdBs.map(db => scala.math.pow(10.0, db/10.0))
-val vars = SNRs.map( snr => 1.0/snr/2 ) //variance for real and imaginay parts (divide by 2)
+  print("Running " + name + " ")
+  
+  val crblist = vars.par.map { v =>
+    val crbvar = crbf().variance(v,L)
+			      print(".")
+			      crbvar //last thing is what gets returned
+			    }.toList
 
-val starttime = (new java.util.Date).getTime
-
-for( L <- Ls ) {
-
-  { //BPSK first
-    print("Computing BPSK CRBs L = " + L + " ")
-    
-    val crblist = vars.par.map { v =>
-      val crbvar = (new BPSKCRB).variance(v,L)
-      print(".")
-      crbvar //last thing is what gets returned
-    }.toList
-
-    val file = new java.io.FileWriter("data/BPSKCRB" + L)
-      (crblist, SNRdBs).zipped.foreach{ (crb, snr) =>
-        file.write(snr.toString.replace('E', 'e') + "\t" + crb.toString.replace('E', 'e')  + "\n")
-      }
-    file.close;
-    println(" finished")
-  }
+  val file = new java.io.FileWriter("data/" + name)
+  (crblist, snrdbs).zipped.foreach{ (crb, snr) => file.write(snr.toString.replace('E', 'e') + "\t" + crb.toString.replace('E', 'e')  + "\n") }
+  file.close
+  println(" finished")
 
 }
 
+val Ls = List(32,256,2048,4096) //number of symbols
+val SNRdBs = -20.0 to 20.0 by 0.5 //snrs we will use
+
+//list of CRBS we will compute with names
+val crbfs = List[(()=>CramerRaoBound, String)](
+( () => new BPSKCRB, "BPSKCRB" ),
+( () => new QPSKCRB, "QPSKCRB" )
+)
+
+val starttime = (new java.util.Date).getTime
+
+//run everything
+for( L <- Ls; crbf <- crbfs ) computecrb(crbf._1, L, SNRdBs, crbf._2 + "" + L.toString)
 
 val runtime = (new java.util.Date).getTime - starttime
-
 println("Finished in " + (runtime/1000.0) + " seconds.\n")
 
 
--- code/scala/src/cam/psk/Bounds.scala
+++ code/scala/src/cam/psk/Bounds.scala
@@ -8,6 +8,12 @@
 package cam.psk
 
 import numbers.finite.integration.RealIntegral
+import scala.math.exp
+import scala.math.Pi
+import scala.math.sqrt
+import scala.math.sinh
+import scala.math.cosh
+import scala.math.tanh
 
 trait CramerRaoBound {
   /** Returns a bound on the estimator variance given noise variance v and number of symbols L*/
@@ -15,7 +21,7 @@ trait CramerRaoBound {
 }
 
 /** 
- * This is the bound derived by Bill Cowley.
+ * This is the bound derived by Bill Cowley for BPSK
  */
 class BPSKCRB extends CramerRaoBound {
   
@@ -25,8 +31,29 @@ class BPSKCRB extends CramerRaoBound {
   }
   
   private def f(v : Double, n : Double) : Double = {
-    val tan = scala.math.tanh((1+n)/v)
-    val exp = scala.math.exp(-n*n/2/v)
-    return tan*tan*exp/scala.math.sqrt(2*scala.math.Pi*v)
+    val tan1 = tanh((1+n)/v)
+    val exp1 = exp(-n*n/2/v)/sqrt(2*Pi*v)
+    return tan1*tan1*exp1
+  }
+}
+
+/** 
+ * This is the bound derived by Bill Cowley for QPSK
+ */
+class QPSKCRB extends CramerRaoBound {
+  
+  final def variance(v : Double, L : Int) : Double = {  
+    def Fqi(nq : Double) = RealIntegral.trapezoidal( ni => f(v,ni, nq), -20.0*sqrt(v), 20.0*sqrt(v), 1000)
+    val Fq = RealIntegral.trapezoidal( nq => Fqi(nq), -20.0*sqrt(v), 20.0*sqrt(v), 1000)
+    return v/L/Fq
+  }
+  
+  private def f(v : Double, ni : Double, nq : Double) : Double = {
+    val sinhi = sinh((1+ni)/v)*nq
+    val sinhq = sinh(nq/v)*(1+ni)
+    val cosh1 = cosh((1+ni)/v) + cosh(nq/v)
+    val exp1 = scala.math.exp(-(ni*ni+nq*nq)/2/v)/(2*scala.math.Pi*v)
+    val a = (sinhi - sinhq)/cosh1
+    return a * a * exp1 / v
   }
 }
